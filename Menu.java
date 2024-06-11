e69e827e4d58f393b34c3457f94a15d6ce384905
Tiago Silva <servidortiago123@gmail.com>
2024-06-11 15:39:01 -0300

    aa

--- Menu.java
+++ Menu.java
@@ -18,31 +18,17 @@ class GeometriaPlana {
     }
 }
 
-class GeometriaEspacial {
-    double calcularVolumeCubo(double lado) {
-        return Math.pow(lado, 3);
-    }
 
-    double calcularVolumeParalelepipedo(double comprimento, double largura, double altura) {
-        return comprimento * largura * altura;
-    }
-
-    double calcularVolumeEsfera(double raio) {
-        return (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
-    }
-}
 
 public class Menu {
     private final GeometriaPlana geometriaPlana = new GeometriaPlana();
-    private final GeometriaEspacial geometriaEspacial = new GeometriaEspacial();
     private final Scanner scanner = new Scanner(System.in);
 
     public void exibirMenu() {
         while (true) {
             System.out.println("\nMenu:");
             System.out.println("1. Calcular área de figuras planas");
-            System.out.println("2. Calcular volume de figuras espaciais");
-            System.out.println("3. Sair");
+            System.out.println("2. Sair");
             System.out.print("Escolha uma opção: ");
             String escolha = scanner.nextLine();
 
@@ -106,42 +92,7 @@ public class Menu {
         }
     }
 
-    private void menuGeometriaEspacial() {
-        System.out.println("\nFiguras espaciais:");
-        System.out.println("1. Cubo");
-        System.out.println("2. Paralelepípedo");
-        System.out.println("3. Esfera");
-        System.out.print("Escolha uma figura: ");
-        String figura = scanner.nextLine();
 
-        double resultado;
-        switch (figura) {
-            case "1":
-                System.out.print("Digite o lado do cubo: ");
-                double ladoCubo = Double.parseDouble(scanner.nextLine());
-                resultado = geometriaEspacial.calcularVolumeCubo(ladoCubo);
-                System.out.println("Volume do cubo: " + resultado);
-                break;
-            case "2":
-                System.out.print("Digite o comprimento do paralelepípedo: ");
-                double comprimentoParalelepipedo = Double.parseDouble(scanner.nextLine());
-                System.out.print("Digite a largura do paralelepípedo: ");
-                double larguraParalelepipedo = Double.parseDouble(scanner.nextLine());
-                System.out.print("Digite a altura do paralelepípedo: ");
-                double alturaParalelepipedo = Double.parseDouble(scanner.nextLine());
-                resultado = geometriaEspacial.calcularVolumeParalelepipedo(comprimentoParalelepipedo, larguraParalelepipedo, alturaParalelepipedo);
-                System.out.println("Volume do paralelepípedo: " + resultado);
-                break;
-            case "3":
-                System.out.print("Digite o raio da esfera: ");
-                double raioEsfera = Double.parseDouble(scanner.nextLine());
-                resultado = geometriaEspacial.calcularVolumeEsfera(raioEsfera);
-                System.out.println("Volume da esfera: " + resultado);
-                break;
-            default:
-                System.out.println("Opção inválida.");
-        }
-    }
 
     public static void main(String[] args) {
         Menu menu = new Menu();
